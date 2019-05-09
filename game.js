f4d8eaba478c83ff030cf1a14f7f59f54475769f
Hanjun Kim <hallazzang@gmail.com>
2019-05-09 23:48:01 +0900

    make member variables private

--- game.js
+++ game.js
@@ -10,41 +10,53 @@
         loop = () => {},
       } = config;
 
-      this.canvas = document.createElement('canvas');
-      this.canvas.width = canvasWidth;
-      this.canvas.height = canvasHeight;
-      this.canvas.tabIndex = 0;
-      // this.canvas.style.outline = 'none';
-      this.ctx = this.canvas.getContext('2d');
+      this._canvas = document.createElement('canvas');
+      this._canvas.width = canvasWidth;
+      this._canvas.height = canvasHeight;
+      this._canvas.tabIndex = 0;
+      // this._canvas.style.outline = 'none';
+      this._ctx = this._canvas.getContext('2d');
 
-      this.loop = loop.bind(this);
+      this._loop = loop.bind(this);
 
-      this.pressedKeys = new Set();
-      this.canvas.addEventListener('keydown', e => {
-        this.pressedKeys.add(e.key);
+      this._pressedKeys = new Set();
+      this._canvas.addEventListener('keydown', e => {
+        this._pressedKeys.add(e.key);
       });
-      this.canvas.addEventListener('keyup', e => {
-        this.pressedKeys.delete(e.key);
+      this._canvas.addEventListener('keyup', e => {
+        this._pressedKeys.delete(e.key);
       });
 
-      this.mousePos = {x: 0, y: 0};
-      this.isMousePressed = false;
-      this.canvas.addEventListener('pointerdown', e => {
-        this.isMousePressed = true;
-        this.canvas.setPointerCapture(e.pointerId);
+      this._mousePos = {x: 0, y: 0};
+      this._isMousePressed = false;
+      this._canvas.addEventListener('pointerdown', e => {
+        this._isMousePressed = true;
+        this._canvas.setPointerCapture(e.pointerId);
       });
-      this.canvas.addEventListener('pointerup', e => {
-        this.isMousePressed = false;
-        this.canvas.releasePointerCapture(e.pointerId);
+      this._canvas.addEventListener('pointerup', e => {
+        this._isMousePressed = false;
+        this._canvas.releasePointerCapture(e.pointerId);
       });
-      this.canvas.addEventListener('pointermove', e => {
-        this.mousePos.x = Math.min(this.canvas.width, Math.max(0, e.offsetX));
-        this.mousePos.y = Math.min(this.canvas.height, Math.max(0, e.offsetY));
+      this._canvas.addEventListener('pointermove', e => {
+        this._mousePos.x = Math.min(this._canvas.width, Math.max(0, e.offsetX));
+        this._mousePos.y = Math.min(this._canvas.height, Math.max(0, e.offsetY));
       });
 
       if (el) {
         this.mount(el);
       }
+    }
+
+    get pressedKeys() {
+      return this._pressedKeys;
+    }
+
+    get isMousePressed() {
+      return this._isMousePressed;
+    }
+
+    get mousePos() {
+      return this._mousePos;
     }
 
     mount(el) {
@@ -54,14 +66,14 @@
       } else {
         elem = el;
       }
-      elem.replaceWith(this.canvas);
+      elem.replaceWith(this._canvas);
     }
 
     run() {
       let previousTick = undefined;
       const wrappedLoop = t => {
         if (previousTick !== undefined) {
-          this.loop(t / 1000, (t - previousTick) / 1000);
+          this._loop(t / 1000, (t - previousTick) / 1000);
         }
         previousTick = t;
         requestAnimationFrame(wrappedLoop);
